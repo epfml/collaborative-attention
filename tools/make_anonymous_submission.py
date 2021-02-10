5b05408640e339970f1009e06cac678e2a1ab534
Jean-Baptiste Cordonnier <jbcdnr@gmail.com>
2021-02-10 20:28:43 +0100

    Update submission script

--- tools/make_anonymous_submission.py
+++ tools/make_anonymous_submission.py
@@ -1,16 +1,27 @@
 import pathlib
 import shutil
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument("--supplementary_pdf", type=str)
+args = parser.parse_args()
+supplementary = pathlib.Path(args.supplementary_pdf) if args.supplementary_pdf else None
+assert supplementary.exists() and supplementary.is_file()
 
-root = pathlib.Path(__file__).parent / ".."
+root = (pathlib.Path(__file__).parent / "..")
 
 ignore_file_extensions = ["png", "gif"]
+forbidden_words = ["jb", "cordonnier", "loukas", "jaggi", "epfml", "epfl", "mlbench", "epfml", "github", "arxiv", "eprint"]
+filter_files = [".git", "tools", "__pycache__", ".DS_Store", ".zip"]
 
-def should_be_anonymized(line):
-    return any(
-        (s in line.lower())
-        for s in ["jb", "cordonnier", "loukas", "jaggi", "epfml", "epfl", "mlbench", "epfml", "github", "arxiv", "eprint"]
-    )
+
+def copy_anonymized(source_file, target_file):
+    with open(source_file, "r") as inf, open(target_file, "w") as outf:
+        for line in inf:
+            if any(forbidden.lower() in line.lower() for forbidden in forbidden_words):
+                outf.write("#- anonymized\n")
+            else:
+                outf.write(line)
 
 try:
     tmp_directory = root / "tmp"
@@ -19,10 +30,8 @@ try:
 
     tmp_directory.mkdir()
 
-    print(tmp_directory, root)
-
-    filter_files = [".git", "tools", "__pycache__", ".DS_Store", ".zip"]
     files = [f for f in root.glob("**/*") if not any(s in str(f) for s in filter_files)]
+    print(root, files)
 
     for source_file in files:
         target_file = tmp_directory / str(source_file)[3:]
@@ -36,18 +45,20 @@ try:
             shutil.copy(source_file, target_file)
         # Otherwise anonymise the file line by line
         else:
-            with open(source_file, "r") as inf:
-                with open(target_file, "w") as outf:
-                    for line in inf:
-                        if should_be_anonymized(line):
-                            outf.write("#- anonymized\n")
-                        else:
-                            outf.write(line)
+            copy_anonymized(source_file, target_file)
+
+    # If we provide a PDF, we move the content of tmp/ into tmp/code/ and add the PDF
+    if supplementary:
+        tmp_directory.rename(root / "code")
+        tmp_directory.mkdir()
+        (root / "code").rename(tmp_directory / "code")
+        shutil.copy(supplementary, tmp_directory / "supplementary.pdf")
+        print(f"Added PDF file '{supplementary}'.")
 
     # Make the zip archive.
-    zip_file = (root / "code").absolute().resolve()
+    zip_file = (root / "supplementary").absolute().resolve()
     shutil.make_archive(zip_file, 'zip', tmp_directory)
-    print(f"Anonymous code archive saved in '{zip_file}.zip'")
+    print(f"Anonymous supplementary archive saved in '{zip_file}.zip'")
 
 finally:
     # Clean up
